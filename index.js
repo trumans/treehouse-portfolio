90e0765192a331d6d89aec484e20853c3540fabe
Truman Smith <trumanwsmith@gmail.com>
2018-12-16 00:12:31 -0800

    create error page/template. capture not-defined routes and response with 404 error. refactor Back link into an include partial template. open app links in new tab.

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 const express = require('express');
 const app = express();
+const projects = require('./data.json').projects;
 
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
-
 app.listen(3000, () => { console.log('app is listening on port 3000') });
 
-const projects = require('./data.json').projects;
-
-app.use('/about', (req, res) => {
+app.get('/about', (req, res) => {
   console.log("In /about");
   res.render('about');
 });
 
-app.use('/project/:id', (req,res) => {
+app.get('/project/:id', (req, res, next) => {
   console.log('In project route for', req.params.id);
   for (let project of projects) {
     if ( req.params.id === project.id ) {
-      res.render('project', { project });
-      break;
+      return res.render('project', { project });
     }
   }
+  // project isn't in data. return 404
+  next();
 });
 
-app.use('/', (req, res) => {
-  console.log("In root fnc");
+app.get('/', (req, res) => {
+  console.log("In root fcn");
   res.locals.projects = projects;
   res.render('index');
+});
+
+// capture undefined routes to show a 404 message.
+app.use((req, res) => {
+  console.log('In not-defined route');
+  error = new Error('The page was not found');
+  error.statusCode = 404;
+  res.render('error', { error });
+});
+
+// catch errors thrown by app for any request and route
+app.use((err, req, res, next) => {
+  res.status(err.statusCode);
+  // display stack dump on formated error page
+  res.locals.error = err;
+  res.render('error');
 });
